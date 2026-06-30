8eae212e6f61883aab27e945c9823b6e474aa0f1
real-boxer <77096257+ryota-git-ai@users.noreply.github.com>
2026-06-30 16:31:34 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,185 @@
-window.onload=function(){
+// =====================
+// REAL Gallery Slider
+// =====================
 
-console.log(
-"REAL OFFICIAL SITE READY"
+
+let current = 0;
+
+
+
+const slides =
+document.querySelectorAll(".slide");
+
+
+
+const dots =
+document.querySelectorAll(".dot");
+
+
+
+function showSlide(index){
+
+
+
+slides.forEach((slide,i)=>{
+
+
+slide.classList.remove(
+"active",
+"prev",
+"next"
 );
 
 
-};
+
+if(i === index){
+
+
+slide.classList.add("active");
+
+
+}
+
+
+else if(
+i === (index - 1 + slides.length) % slides.length
+){
+
+
+slide.classList.add("prev");
+
+
+}
+
+
+else if(
+i === (index + 1) % slides.length
+){
+
+
+slide.classList.add("next");
+
+
+}
+
+
+
+});
+
+
+
+
+
+dots.forEach((dot,i)=>{
+
+
+dot.classList.remove("active");
+
+
+if(i === index){
+
+
+dot.classList.add("active");
+
+
+}
+
+
+});
+
+
+
+
+}
+
+
+
+
+
+
+
+function nextSlide(){
+
+
+current =
+(current + 1) % slides.length;
+
+
+showSlide(current);
+
+
+}
+
+
+
+
+
+
+function prevSlide(){
+
+
+current =
+(current - 1 + slides.length)
+% slides.length;
+
+
+showSlide(current);
+
+
+}
+
+
+
+
+
+
+
+document
+.querySelector(".next")
+.addEventListener(
+"click",
+nextSlide
+);
+
+
+
+
+
+
+document
+.querySelector(".prev")
+.addEventListener(
+"click",
+prevSlide
+);
+
+
+
+
+
+
+
+
+// 自動スライド
+
+
+setInterval(()=>{
+
+
+nextSlide();
+
+
+},5000);
+
+
+
+
+
+
+
+
+
+// 初期表示
+
+
+showSlide(current);
